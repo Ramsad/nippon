2d0072702a0d7f73a62c1dca1cea88be3e736208
ramsad <ramsad7@gmail.com>
2021-12-26 21:20:20 +0530

    Registration Number Manual Entry

--- karate_management/models/karate_management.py
+++ karate_management/models/karate_management.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api , _
 class KarateManagement(models.Model):
     _inherit = 'res.partner'
 
-    reg_no = fields.Char(string="Reg No", required=False, )
+    reg_no = fields.Char(string="Reg No", required=False, default="NKCD")
     registration_date = fields.Date(string="Registration Date", required=False,default=fields.Date.today() )
     dob = fields.Date(string="Date of Birth", required=False, )
     age = fields.Integer(string="Age", required=False, compute='calculate_age')
@@ -20,7 +20,7 @@ class KarateManagement(models.Model):
 
     @api.model
     def create(self, values):
-        values['reg_no'] = self.env['ir.sequence'].next_by_code('seq.membership') or _('New')
+        # values['reg_no'] = self.env['ir.sequence'].next_by_code('seq.membership') or _('New')
         return super(KarateManagement, self).create(values)
 
 
